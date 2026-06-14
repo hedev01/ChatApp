6f56702bea4e23aa80fed9d4b4f86c32f991fbcd
Hedeve01 <nazari.fph@gmail.com>
2026-06-14 21:52:04 +0330

    Feat: Add App Colors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Light
+  static const lightBackground = Color(0XFFF7F8FA);
+  static const lightbackgroundButtonColor = Color(0XFF141718);
+}
