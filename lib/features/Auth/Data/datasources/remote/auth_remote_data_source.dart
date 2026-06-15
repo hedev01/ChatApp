b73c2532420462c0afd79dc3758e5d188ba6a026
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:35:33 +0330

    Refactor: change struc

--- lib/features/Auth/Data/datasources/remote/auth_remote_data_source.dart
+++ lib/features/Auth/Data/datasources/remote/auth_remote_data_source.dart
@@ -1,25 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:chat_app/core/constans/constans.dart';
 import 'package:chat_app/features/Auth/Data/models/user_model.dart';
 import 'package:chat_app/features/Auth/Data/models/user_request.dart';
-import 'package:http/http.dart' as http;
 
-class AuthRemoteDataSource {
-  final http.Client client;
-  AuthRemoteDataSource(this.client);
-
-  Future<UserModel> register(UserRequestModel request) async {
-    final response = await http.post(
-      Uri.parse("${Constans.baseUrl}/api/User/Register"),
-      body: request.toJson(),
-    );
-
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-
-      return UserModel.fromJson(json["Data"]);
-    }
-    throw Exception('Register Failed : ${response.statusCode}');
-  }
+abstract class AuthRemoteDataSource {
+  Future<UserModel?> register(UserRequestModel request);
 }
---
+++ lib/features/Auth/Data/datasources/remote/auth_remote_data_source_imp.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:chat_app/features/Auth/Data/datasources/remote/auth_remote_data_source.dart';
+
+import '../../../../../core/constans/constans.dart';
+import '../../models/user_model.dart';
+import '../../models/user_request.dart';
+import 'package:http/http.dart' as http;
+
+class AuthRemoteDataSourceImp implements AuthRemoteDataSource {
+  @override
+  Future<UserModel?> register(UserRequestModel request) async {
+      try {
+      final response = await http
+          .post(
+            Uri.parse("${Constans.baseUrl}/api/User/Register"),
+            headers: {"Content-Type": "application/json"},
+            body: jsonEncode(request.toJson()),
+          )
+          .timeout(Duration(seconds: Constans.timeOut));
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+
+        return UserModel.fromJson(json);
+      }
+    } on TimeoutException {
+      throw TimeoutException(
+        "${Constans.baseUrl}/api/User/Register"
+        " TimeOut Exception",
+      );
+    } catch (e) {
+      throw Exception('Register Failed : $e');
+    }
+    return null;
+  }
+}
