9b0b005c06a0061f3c9355ee1d1f7ea3cc56c55c
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:37:34 +0330

    Refactor: Register Page

--- lib/features/Auth/presentation/pages/register_page.dart
+++ lib/features/Auth/presentation/pages/register_page.dart
@@ -1,12 +1,40 @@
+import 'package:chat_app/core/theme/app_colors.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register_bloc.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register_event.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register_state.dart';
+import 'package:chat_app/features/Auth/presentation/widgets/register_title.dart';
+import 'package:chat_app/global_widget/error_message_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 import '../../../../global_widget/app_text_field.dart';
+import '../../../../global_widget/password_text_field.dart';
 import '../widgets/auth_button.dart';
 import '../widgets/auth_footer.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  late final TextEditingController userNameCtr;
+  late final TextEditingController firstNameCtr;
+  late final TextEditingController lastNameCtr;
+  late final TextEditingController emailCtr;
+  late final TextEditingController passwordCtr;
+  @override
+  void initState() {
+    userNameCtr = TextEditingController();
+    firstNameCtr = TextEditingController();
+    lastNameCtr = TextEditingController();
+    emailCtr = TextEditingController();
+    passwordCtr = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,27 +53,71 @@ class RegisterPage extends StatelessWidget {
                 ),
                 children: [
                   SizedBox(height: size.height * .06),
-                  Text(
-                    "Create Your\n Account",
-                    style: TextStyle(fontSize: 35, color: Color(0xff323142)),
-                  ),
+                  RegisterTitle(),
                   SizedBox(height: size.height * .05),
-                  const AppTextField(hint: "User Name", icon: Icons.person),
-                  SizedBox(height: size.height * .02),
-                  const AppTextField(hint: "First Name", icon: Icons.person),
-                  SizedBox(height: size.height * .02),
-                  const AppTextField(hint: "Last Name", icon: Icons.person),
-                  SizedBox(height: size.height * .02),
-                  const AppTextField(hint: "Email", icon: Icons.mail),
-                  SizedBox(height: size.height * .02),
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock, size: 25),
-                      hintText: "Password",
-                    ),
+                  AppTextField(
+                    hint: "User Name",
+                    icon: Icons.person,
+                    controller: userNameCtr,
                   ),
                   SizedBox(height: size.height * .02),
-                  AuthButton(size: size),
+                  AppTextField(
+                    hint: "First Name",
+                    icon: Icons.person,
+                    controller: firstNameCtr,
+                  ),
+                  SizedBox(height: size.height * .02),
+                  AppTextField(
+                    hint: "Last Name",
+                    icon: Icons.person,
+                    controller: lastNameCtr,
+                  ),
+                  SizedBox(height: size.height * .02),
+                  AppTextField(
+                    hint: "Email",
+                    icon: Icons.mail,
+                    controller: emailCtr,
+                  ),
+                  SizedBox(height: size.height * .02),
+                  PasswordTextField(passwordCtr: passwordCtr),
+                  SizedBox(height: size.height * .02),
+                  BlocBuilder<RegisterBloc, RegisterState>(
+                    builder: (context, state) {
+                      if (state.status == RegisterStatus.success) {
+                        print("success");
+                      }
+                      return AuthButton(
+                        size: size,
+                        onPressed: () {
+                          context.read<RegisterBloc>().add(
+                            RegisterSubmitted(
+                              username: userNameCtr.text,
+                              email: emailCtr.text,
+                              password: passwordCtr.text,
+                              firstName: firstNameCtr.text,
+                              lastName: lastNameCtr.text,
+                            ),
+                          );
+                        },
+                        child: state.status == RegisterStatus.loading
+                            ? CircularProgressIndicator(
+                                backgroundColor: AppColors.lightBackground,
+                              )
+                            : Text("Register"),
+                      );
+                    },
+                  ),
+                  BlocBuilder<RegisterBloc, RegisterState>(
+                    builder: (context, state) {
+                      if (state.status == RegisterStatus.failure) {
+                        return ErrorMessageWidget(
+                          message: state.error ?? "Error",
+                        );
+                      }
+
+                      return SizedBox();
+                    },
+                  ),
                   SizedBox(height: size.height * .02),
                   AuthFooter(onTap: () {}),
                 ],
@@ -55,5 +127,15 @@ class RegisterPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    userNameCtr.dispose();
+    firstNameCtr.dispose();
+    lastNameCtr.dispose();
+    emailCtr.dispose();
+    passwordCtr.dispose();
+    super.dispose();
   }
 }
