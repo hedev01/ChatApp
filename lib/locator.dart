89883b95d51e9ae1843288ccc756b2a9da44e7f0
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:34:36 +0330

    Feat: Add DI

---
+++ lib/locator.dart
@@ -0,0 +1,30 @@
+
+
+import 'package:chat_app/features/Auth/Data/datasources/local/auth_local_data_source.dart';
+import 'package:chat_app/features/Auth/Data/datasources/local/auth_local_data_source_imp.dart';
+import 'package:chat_app/features/Auth/Data/datasources/remote/auth_remote_data_source.dart';
+import 'package:chat_app/features/Auth/Data/datasources/remote/auth_remote_data_source_imp.dart';
+import 'package:chat_app/features/Auth/Data/repositories/auth_repository_imp.dart';
+import 'package:chat_app/features/Auth/domain/repositories/auth_repository.dart';
+import 'package:chat_app/features/Auth/domain/usecases/auth_usecase.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt locator = GetIt.instance;
+
+void setup() {
+
+  locator.registerSingleton<AuthRemoteDataSource>(AuthRemoteDataSourceImp());
+  locator.registerSingleton<AuthLocalDataSource>(AuthLocalDataSourceImp());
+
+  /// repositories
+  locator.registerSingleton<AuthRepository>(
+    AuthRepositoryImp(locator(), locator()),
+  );
+
+  ///UseCase
+  locator.registerSingleton(AuthUseCase(locator()));
+
+  ///Bloc
+  locator.registerSingleton(RegisterBloc(locator()));
+}
