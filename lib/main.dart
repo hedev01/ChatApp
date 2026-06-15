f22a23763184b45aad671ca9632885f2e1f39044
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:34:23 +0330

    Feat: Register Blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/core/theme/app_theme.dart';
+import 'package:chat_app/features/Auth/Data/repositories/auth_repository_imp.dart';
+import 'package:chat_app/features/Auth/domain/repositories/auth_repository.dart';
+import 'package:chat_app/features/Auth/domain/usecases/auth_usecase.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register_bloc.dart';
+import 'package:chat_app/features/Auth/presentation/cubit/password_visibility_cubit.dart';
 import 'package:chat_app/features/Auth/presentation/pages/register_page.dart';
+import 'package:chat_app/global_widget/error_message_widget.dart';
+import 'package:chat_app/locator.dart';
 import 'package:chat_app/signalr_client.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+ // await Hive.initFlutter();
+  setup();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => PasswordVisibilityCubit()),
+        BlocProvider(create: (_) => RegisterBloc(locator.get())),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
