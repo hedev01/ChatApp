18d283836fc128d0a2e9f5d7e93d1cd936bb2c39
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:36:44 +0330

    Feat: Add Save User

--- lib/features/Auth/domain/repositories/auth_repository.dart
+++ lib/features/Auth/domain/repositories/auth_repository.dart
@@ -2,5 +2,6 @@ import 'package:chat_app/features/Auth/domain/entities/register_request_entity.d
 import 'package:chat_app/features/Auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepository {
-Future<UserEntity> register(RegisterRequestEntity request);
+  Future<UserEntity?> register(RegisterRequestEntity request);
+  Future<void> saveUser(UserDataEntity user);
 }
