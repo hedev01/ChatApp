1e6bc7e69418ae574a9148cc38c4212233a5f440
Hedeve01 <nazari.fph@gmail.com>
2026-06-16 00:35:14 +0330

    Feat: Add Hive

---
+++ lib/features/Auth/Data/datasources/local/auth_local_data_source.dart
@@ -0,0 +1,5 @@
+import 'package:chat_app/features/Auth/Data/models/user_model.dart';
+
+abstract class AuthLocalDataSource {
+  Future<void> saveUser(UserDataModel user);
+}
---
+++ lib/features/Auth/Data/datasources/local/auth_local_data_source_imp.dart
@@ -0,0 +1,13 @@
+import 'package:chat_app/features/Auth/Data/datasources/local/auth_local_data_source.dart';
+import 'package:chat_app/features/Auth/Data/models/user_model.dart';
+import 'package:hive/hive.dart';
+
+class AuthLocalDataSourceImp implements AuthLocalDataSource {
+  static const String boxName = "auth";
+  @override
+  Future<void> saveUser(UserDataModel user) async {
+    final box = await Hive.openBox(boxName);
+
+    await box.put('user', user.toJson());
+  }
+}
