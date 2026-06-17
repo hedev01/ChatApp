5a4273cce59f04d7959fe87e909046c4ca25537a
Hedeve01 <nazari.fph@gmail.com>
2026-06-17 22:59:03 +0330

    Feat: Add New DI

--- lib/locator.dart
+++ lib/locator.dart
@@ -7,13 +7,14 @@ import 'package:chat_app/features/Auth/Data/datasources/remote/auth_remote_data_
 import 'package:chat_app/features/Auth/Data/repositories/auth_repository_imp.dart';
 import 'package:chat_app/features/Auth/domain/repositories/auth_repository.dart';
 import 'package:chat_app/features/Auth/domain/usecases/auth_usecase.dart';
-import 'package:chat_app/features/Auth/presentation/bloc/register_bloc.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/login/login_bloc.dart';
+import 'package:chat_app/features/Auth/presentation/bloc/register/register_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
 
 void setup() {
-
+  /// data source
   locator.registerSingleton<AuthRemoteDataSource>(AuthRemoteDataSourceImp());
   locator.registerSingleton<AuthLocalDataSource>(AuthLocalDataSourceImp());
 
@@ -27,4 +28,5 @@ void setup() {
 
   ///Bloc
   locator.registerSingleton(RegisterBloc(locator()));
+  locator.registerSingleton(LoginBloc(locator()));
 }
